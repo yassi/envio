272298a3e559b37657ebac37e2d8029142d1827d
Yasser Toruno <ytoruno@gmail.com>
2018-09-03 00:58:23 -0400

    Add setup.py for use with setup tools for package building

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="envio",
+    version="1.0.0",
+    author="Yasser Toruno",
+    description="Environment variable parser library for python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yassi/envio",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.5",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ],
+)
